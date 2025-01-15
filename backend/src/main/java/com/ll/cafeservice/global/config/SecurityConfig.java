64d9c9a1efcbf7a36b297c82cce359634745767c
sangxxjin <sj_0368@naver.com>
2025-01-15 11:07:26 +0900

    feat: 비밀번호 암호화 Bean 등록

--- backend/src/main/java/com/ll/cafeservice/global/config/SecurityConfig.java
+++ backend/src/main/java/com/ll/cafeservice/global/config/SecurityConfig.java
@@ -1,28 +1,33 @@
 package com.ll.cafeservice.global.config;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(authorize -> authorize
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .authorizeHttpRequests(authorize -> authorize
 
-                        .anyRequest().permitAll() // 모든 경로 허용
-                );
+                .anyRequest().permitAll() // 모든 경로 허용
+            );
         return http.build();
     }
 
