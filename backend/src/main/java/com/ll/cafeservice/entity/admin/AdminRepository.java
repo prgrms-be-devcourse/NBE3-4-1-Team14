1b5577afdc30427914024a03c2e40303499642b5
sangxxjin <sj_0368@naver.com>
2025-01-15 11:09:10 +0900

    feat: existsByUsername

--- backend/src/main/java/com/ll/cafeservice/entity/admin/AdminRepository.java
+++ backend/src/main/java/com/ll/cafeservice/entity/admin/AdminRepository.java
@@ -1,8 +1,7 @@
 package com.ll.cafeservice.entity.admin;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Optional<Admin> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
