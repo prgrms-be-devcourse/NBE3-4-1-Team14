31883ead2dcdfb43742b33649018f69fcf28a6a2
ChoiHyunSan <chsan626@gmail.com>
2025-01-17 15:29:37 +0900

    feat: 주문에 대한 처리 시, 관련 이미지 데이터의 로직이 동작

--- backend/src/main/java/com/ll/cafeservice/domain/product/service/ProductService.java
+++ backend/src/main/java/com/ll/cafeservice/domain/product/service/ProductService.java
@@ -8,6 +8,7 @@ import com.ll.cafeservice.domain.product.dto.response.ProductCreateResponse;
 import com.ll.cafeservice.domain.product.dto.response.ProductDeleteResponse;
 import com.ll.cafeservice.domain.product.dto.response.ProductInfoResponse;
 import com.ll.cafeservice.domain.product.dto.response.ProductUpdateResponse;
+import com.ll.cafeservice.domain.product.implement.ProductImageManager;
 import com.ll.cafeservice.domain.product.implement.ProductManager;
 import com.ll.cafeservice.domain.product.implement.ProductReader;
 
@@ -23,20 +24,25 @@ public class ProductService {
 
     private final ProductManager productManager;
     private final ProductReader productReader;
+    private final ProductImageManager productImageManager;
 
     public ProductCreateResponse addProduct(final ProductCreateRequest request) {
-    log.info("{} {} {} {}", request.name(), request.price(), request.description(), request.quantity());
+
+        // log.info("{} {} {} {}", request.name(), request.price(), request.description(), request.quantity());
+
+        // 품목 이미지 파일 저장
+        String filename = productImageManager.storeProductImage(request.image());
+
         //Request 정보를 이용하여 새로운 품목 (NewProduct 클래스)를 생성
         NewProduct newProduct = new NewProduct(
                 request.name(),
                 request.price(),
                 request.description(),
                 request.quantity(),
-                ""
+                filename
         );
 
         //ProductManager 클래스에 새로움 품목을 저장하도록 요청
-
         Long savedProductId = productManager.addProduct(newProduct);
 
         //품목 저장에 대한 요청 반환
@@ -45,29 +51,36 @@ public class ProductService {
 
 
     public List<ProductInfoResponse> getList() {
-        return productReader.findAll().stream()
+        return productReader.findAllActivateProduct().stream()
                 .map(product -> ProductInfoResponse.builder() // 빌더 패턴 사용
                         .id(product.getId())
                         .name(product.getName())
                         .price(product.getPrice())
                         .description(product.getDescription())
-                        .imageUrl(product.getImageUrl())
+                        .filename(product.getImgFilename())
                         .build()
                 )
                 .collect(Collectors.toList());
     }
 
     public ProductUpdateResponse updatedProduct(Long id, ProductUpdateRequest request) {
+
         //제품 데이터 조회
         Product existingProduct = productReader.findById(id);
+
+        // 기존 이미지 삭제 및 새로운 이미지 저장
+        productImageManager.deleteProductImageByFilename(existingProduct.getImgFilename());
+        String newImageFilename = productImageManager.storeProductImage(request.image());
 
         //요청 데이터를 이용하여 기존 제품 정보 수정
        Product updatedProduct = existingProduct.updateProduct(
                 request.name(),
                 request.price(),
                 request.description(),
-                request.quantity()
+                request.quantity(),
+               newImageFilename
         );
+
         //수정된 제품 저장
         productManager.updatedProduct(updatedProduct);
 
@@ -76,10 +89,15 @@ public class ProductService {
 
 
     public ProductDeleteResponse deleteProduct(Long id) {
+
         // 제품 조회
         Product existingProduct = productReader.findById(id);
-        // 제품 삭제
-        productManager.deletedProduct(existingProduct);
+
+        // 제품 이미지 삭제
+        productImageManager.deleteProductImageByFilename(existingProduct.getImgFilename());
+
+        // 제품 비활성화
+        productManager.deactivateProduct(existingProduct);
 
         // 삭제 완료 응답
         return new ProductDeleteResponse(id, "제품이 삭제되었습니다.");
