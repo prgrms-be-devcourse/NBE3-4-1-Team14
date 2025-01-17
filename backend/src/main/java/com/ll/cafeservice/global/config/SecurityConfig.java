194bb06bb40a3121dc316f771d9ed1e002022967
sangxxjin <sj_0368@naver.com>
2025-01-18 03:36:04 +0900

    feat: 인증 경로 설정

--- backend/src/main/java/com/ll/cafeservice/global/config/SecurityConfig.java
+++ backend/src/main/java/com/ll/cafeservice/global/config/SecurityConfig.java
@@ -1,18 +1,31 @@
 package com.ll.cafeservice.global.config;
 
+import com.ll.cafeservice.global.exception.CustomAccessDeniedHandler;
+import com.ll.cafeservice.global.exception.CustomAuthenticationEntryPoint;
+import com.ll.cafeservice.global.jwt.JwtAuthenticationFilter;
+import com.ll.cafeservice.global.jwt.JwtProvider;
 import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -25,8 +38,9 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(authorize -> authorize
-
-                .anyRequest().permitAll() // 모든 경로 허용
+                .requestMatchers("/api/*/admin/login").permitAll()
+                .requestMatchers("/api/*/admin/**").authenticated()
+                .anyRequest().permitAll()
             );
         return http.build();
     }
@@ -34,7 +48,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000" , "http://localhost:8080" ));
+        configuration.setAllowedOrigins(
+            Arrays.asList("http://localhost:3000", "http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
@@ -44,5 +59,4 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
