5dd4e7321aa0dad30fb38193bd656abd76fe1caf
sangxxjin <sj_0368@naver.com>
2025-01-17 12:44:04 +0900

    feat: 토큰 발급
    본문: 로그인 성공 했을때 토큰을 같이 발급해서 반환값으로 구현
    
    차후에 쿠키에 저장 예정

--- backend/src/main/java/com/ll/cafeservice/domain/admin/controller/v1/AdminControllerV1.java
+++ backend/src/main/java/com/ll/cafeservice/domain/admin/controller/v1/AdminControllerV1.java
@@ -3,31 +3,41 @@ package com.ll.cafeservice.domain.admin.controller.v1;
 import com.ll.cafeservice.api.Result;
 import com.ll.cafeservice.domain.admin.dto.request.LoginRequest;
 import com.ll.cafeservice.domain.admin.dto.response.LoginResponse;
+import com.ll.cafeservice.domain.admin.service.AdminService;
+import com.ll.cafeservice.entity.admin.Admin;
+import com.ll.cafeservice.global.jwt.JwtProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "UserControllerV1", description = "API 유저관련 컨트롤러")
+@Tag(name = "AdminControllerV1", description = "API 관리자 관련 컨트롤러")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/admin")
 public class AdminControllerV1 {
 
+    private final AdminService adminService;
+    private final JwtProvider jwtProvider;
+
     @PostMapping("/login")
-    @Operation(summary = "로그인", description = "")
-    public Result<LoginResponse> login(
-            @RequestBody @Valid LoginRequest request
-    ) {
-        log.info("login request: {}", request.username());
-        return Result.success(new LoginResponse(request.username()));
+    @Operation(summary = "로그인", description = "관리자 로그인 API")
+    public Result<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+        Admin admin = adminService.authenticate(request.username(), request.password());
+        String token = jwtProvider.generateToken(admin.getUsername());
+        LoginResponse response = new LoginResponse(admin.getUsername(), token);
+        return Result.success(response);
     }
 
-
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
