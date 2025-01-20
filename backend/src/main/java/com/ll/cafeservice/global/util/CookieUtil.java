73ba4b0f6b57ebc96ecbdffea9495535d604d286
sangxxjin <sj_0368@naver.com>
2025-01-20 17:40:57 +0900

    feat: cookie util

---
+++ backend/src/main/java/com/ll/cafeservice/global/util/CookieUtil.java
@@ -0,0 +1,26 @@
+package com.ll.cafeservice.global.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieUtil {
+
+    public static void addCookie(HttpServletResponse response, String name, String value,
+        int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    public static void removeCookie(HttpServletResponse response, String name) {
+        Cookie cookie = new Cookie(name, null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+}
